39e5b08c087f9b9b64c3b87d29ad8e752bdfdf66
sasa0xx <vole2019123@gmail.xom>
2025-12-25 16:12:11 +0000

    read logs file and created new files

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	file, err := os.Open("logs.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
 }
